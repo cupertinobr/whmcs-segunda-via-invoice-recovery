ddbf9a72b3d1ec98c4fe952d17a5f17257615865
Rodolfo <rodolfols@gmail.com>
2026-06-03 19:10:57 -0300

    fix: restore default PHP error handler to prevent unexpected 500 errors

--- segunda-via.php
+++ segunda-via.php
@@ -2,6 +2,9 @@
 define("CLIENTAREA", true);
 require_once __DIR__ . '/init.php';
 
+// Restore the default PHP error handler to prevent WHMCS from converting notices/warnings to HTTP 500
+while (restore_error_handler());
+
 register_shutdown_function(function() {
     $error = error_get_last();
     if ($error === null || !in_array($error['type'] ?? 0, [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
